0057e6c5a6cf0bafe4f9bf956d844e4614066d27
“Snappyhacker” <jimodhiambo515@gmail.com>
2024-09-22 17:18:17 +0300

    initial commit

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,16 @@
 # your code goes here!
+# countdown.py
+
+def countdown(start):
+    while start > 0:
+        print(f"{start} SECOND(S)!")
+        start -= 1
+    print("HAPPY NEW YEAR!")
+
+def countdown_with_sleep(start):
+    import time
+    while start > 0:
+        print(f"{start} SECOND(S)!")
+        time.sleep(1)  # Sleep for 1 second between prints
+        start -= 1
+    print("HAPPY NEW YEAR!")
